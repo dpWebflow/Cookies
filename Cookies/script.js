aa26edb6248226e85f30ea021403a4c60a0e07fb
dpWebflow <134136564+dpWebflow@users.noreply.github.com>
2025-11-03 22:34:08 +0100

    Add files via upload

---
+++ Cookies/script.js
@@ -0,0 +1,23 @@
+document.querySelector('#setCookie').addEventListener('click', e => {
+	e.preventDefault();
+    
+    document.cookie = "name=test1";
+	document.cookie = "name=test2";
+
+	const date = new Date();
+
+	let today = date.getTime();
+	let expires = 14 * 24 * 60 * 60 * 1000;
+
+
+	date.setTime(today + expires);
+
+	let new_date = date.toUTCString();
+
+	document.cookie = `login=1; expires=${new_date}`;
+
+	console.log(document.cookie);
+
+});
+
+
